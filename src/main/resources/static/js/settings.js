94c518e1143ceeeb0de3cf788a4c07bcbb7b33be
capitanfight <cinielia0719@gmail.com>
2025-04-15 19:30:49 +0200

    finished settings.js

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -4,21 +4,46 @@ function redirectHome() {
     window.location.replace('/');
 }
 
+function setAudio(audio) {
+    cookiesHandler.setCookie("audio", audio);
+}
+
+let tempVolume = Number(cookiesHandler.getCookie("audio"))
+
+const volumeSlider = document.getElementById("volumeRange");
+volumeSlider.value = tempVolume === null || tempVolume === undefined ? 50 : tempVolume
+let prevVolume = volumeSlider.value;
+
 let audio = true
 function toggleAudio() {
     audio = !audio
 
-    cookiesHandler.setCookie("audio", audio);
     document.getElementById("audio-logo").setAttribute("src", `/img/svg/audio-${audio ? "on" : "off"}-logo.svg`);
+
+    if (audio) {
+        volumeSlider.value = prevVolume
+    } else {
+        prevVolume = volumeSlider.value;
+        volumeSlider.value = 0;
+    }
+
+    setAudio(volumeSlider.value);
 }
 
-const volumeSlider = document.getElementById("volumeRange");
-
 volumeSlider.addEventListener("input", function () {
-    const volume = this.value / 100;
+    const volume = Number(this.value);
     // Supponendo che tu abbia un elemento <audio> con id="audio"
     const audio = document.getElementById("audio");
     if (audio) audio.volume = volume;
+
+    if (volume === 0)
+        document.getElementById("audio-logo").setAttribute("src", `/img/svg/audio-off-logo.svg`);
+    if (prevVolume === 0 && volume !== 0)
+        document.getElementById("audio-logo").setAttribute("src", `/img/svg/audio-on-logo.svg`);
+
+    prevVolume = volume;
+
+    setAudio(volume)
 });
 
 document.getElementById("close-btn").addEventListener("click", redirectHome);
